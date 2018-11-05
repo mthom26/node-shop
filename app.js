b684459551dc70f0b7bb969d84687722e0b0703c
mthom26 <mthom2626@gmail.com>
2018-11-05 13:10:10 +0000

    Add session

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express');
 const expressHbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const shopRoutes = require('./routes/shop');
 const db = require('./models');
 
 const app = express();
+const sessionStore = new MongoDBStore({
+  uri: process.env.DATABASE_URI,
+  collection: 'sessions'
+});
 
 app.engine('hbs', expressHbs({
   defaultLayout: 'main',
@@ -17,6 +22,21 @@ app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(path.dirname(process.mainModule.filename), 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
+
+app.use(async (req, res, next) => {
+  if(!req.session.user) {
+    // No user on request
+    return next();
+  }
+  // Otherwise find user and attach it to request
+  return next();
+});
 
 app.use(shopRoutes);
 
